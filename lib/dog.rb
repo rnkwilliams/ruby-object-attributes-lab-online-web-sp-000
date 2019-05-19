b7820b8a12c86b550f8ac49912f5e14a143b4f37
Ranika Williams <rnkprice07@gmail.com>
2019-05-19 02:28:53 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,2 +1,17 @@
 class Dog
+  def name
+    @name
+  end
+
+  def name=(dog_name)
+    @name = dog_name
+  end
+
+  def breed
+    @breed
+  end
+
+  def breed=(dog_breed)
+    @breed = breed_breed
+  end
 end
